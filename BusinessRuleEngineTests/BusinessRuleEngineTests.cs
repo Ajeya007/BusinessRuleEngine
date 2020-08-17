b4b25f2f7689e293d82cee6fec0b0141a6cdaba5
Ajeya Bedre <Ajeya.Bedre@quest.com>
2020-08-18 03:30:47 +0530

    Testcases implemented for PaymentProcessSystem

---
+++ BusinessRuleEngineTests/BusinessRuleEngineTests.cs
@@ -0,0 +1,49 @@
+﻿namespace BusinessRuleEngineTests
+{
+    using BusinessRuleEngine.Models;
+    using Xunit;
+
+    public class BusinessRuleEngineTests
+    {
+
+        [Fact]
+        public void Test_PhysicalProduct_ProcessPayment()
+        {
+            Product product = new PhysicalProduct();
+            product.TryProcessPayment(out bool isSuccess);
+            Assert.True(isSuccess);
+        }
+
+        [Fact]
+        public void Test_BookProduct_ProcessPayment()
+        {
+            Product product = new Book();
+            product.TryProcessPayment(out bool isSuccess);
+            Assert.True(isSuccess);
+        }
+
+        [Fact]
+        public void Test_VideoProduct_ProcessPayment()
+        {
+            Product product = new Video();
+            product.TryProcessPayment(out bool isSuccess);
+            Assert.True(isSuccess);
+        }
+
+        [Fact]
+        public void Test_MembershipCreateProduct_ProcessPayment()
+        {
+            Product product = new MembershipCreate();
+            product.TryProcessPayment(out bool isSuccess);
+            Assert.True(isSuccess);
+        }
+
+        [Fact]
+        public void Test_MembershipUpgradeProduct_ProcessPayment()
+        {
+            Product product = new MembershipUpgrade();
+            product.TryProcessPayment(out bool isSuccess);
+            Assert.True(isSuccess);
+        }
+    }
+}
---
+++ BusinessRulesEngineTests/BusinessRuleEngineTestCases.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace BusinessRulesEngineTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NSubstitute;
+    using Shouldly;
+    using Xunit;
+
+    public class BusinessRuleEngineTestCases
+    {
+    }
+}
